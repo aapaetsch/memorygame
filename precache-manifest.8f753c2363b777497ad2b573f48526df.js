3f727c13bf4950e40e5cce58d840dff905160eae
Aidan Paetsch <aapaetsch@gmail.com>
2020-08-21 01:21:04 -0600

    Updates

---
+++ precache-manifest.8f753c2363b777497ad2b573f48526df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61aed1105fe04576ef898a87112c8ffa",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "bf0e55a16dd50e7de97b",
+    "url": "/memorygame/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "7d4b1ab461520c5ad6ba",
+    "url": "/memorygame/static/css/main.120f4b11.chunk.css"
+  },
+  {
+    "revision": "bf0e55a16dd50e7de97b",
+    "url": "/memorygame/static/js/2.c0fe51d7.chunk.js"
+  },
+  {
+    "revision": "43464e588ca9eba5c140d22b7457291f",
+    "url": "/memorygame/static/js/2.c0fe51d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4b1ab461520c5ad6ba",
+    "url": "/memorygame/static/js/main.cd15103b.chunk.js"
+  },
+  {
+    "revision": "d1e0477d5354b8282630",
+    "url": "/memorygame/static/js/runtime-main.792efb20.js"
+  }
+]);
